1aa06ee58f8fdfe1bc2aa08939635bbb19341d7d
Yang Quan <yangquan@trustkernel.com>
2019-06-03 19:36:10 +0800

    release 1

--- c++/udp_tcp/src/common/opfile.cpp
+++ c++/udp_tcp/src/common/opfile.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
 #include <opfiles.h>
+#define SAFE_DELETE(p) {if((p) != NULL) delete [] (p); (p) = NULL;}
 
 FILES* FilesOperation :: get_file_op(char *name) //函数定义 要放到 FILES*  而不要靠近函数名
 {
 	assert(name != NULL);
-	FILES *file = NULL;
+	FILES *files = (FILES*)malloc(sizeof(FILES*)); //结构体指针 必须初始化； 用非指针的方式 不用初始化
+	//FILES files;
 	char data[MAX_DATA_SIZE];
+	printf("file_name = %s\n",name);
 
     if(access(name, F_OK))//判断文件的权限
     {
         printf("####L(%d) file:%s not exist!", __LINE__, name);
-        //exit(0);
+        exit(0);
     }
     
-    file->name = name;
-	FILE *fp = fopen(name, "rb");
-    if(NULL == fp )
+    files->name = name;
+	FILE *fp;
+	fp = fopen(name, "rb");
+    printf("file->data_size = %ld,file->name = %s \n",files->data_size,files->name);
+	if(NULL == fp )
     {
         printf("file:%s can not open!\n", name);
         //exit(0);
     }
    
-	file->data_size = fread(file->data,sizeof(char),MAX_DATA_SIZE,fp);
-    printf("file->data_size = %ld\n",file->data_size);
 
+    printf("file->data_size = %ld\n",files->data_size);
+#if 1
     fseek(fp, 0, SEEK_END); //把文件内部指针移动到文件尾部
-    file->data_size  = ftell(fp); //返回指针偏离文件头的位置(即文件中字符个数)
+    files->data_size  = ftell(fp); //返回指针偏离文件头的位置(即文件中字符个数)
+	printf("file->data_size ftell()= %ld\n",files->data_size);
     fseek(fp, 0, SEEK_SET); //把文件内部指针移回到文件头部
-    
+#endif    
+	//bzero(files->data, MAX_DATA_SIZE);
+	//files->data_size = fread(files->data,sizeof(char),MAX_DATA_SIZE,fp);
+	printf("file->data_size fread()= %ld\n",files->data_size);
     fclose(fp);
-    return file;
+	SAFE_DELETE(files);
+    return files;
+	
 }
-
-
-
-
--- c++/udp_tcp/src/send/send.cpp
+++ c++/udp_tcp/src/send/send.cpp
@@ -5,8 +5,14 @@
 #include <stdlib.h>
 #include <unistd.h> //sleep()
 #include "opfiles.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <assert.h>
 
-#define SAFE_DELETE(p) {if((p) != NULL) delete [] (p); (p) = NULL;}
+#define SEND_FILE "yq.webm"
 
 typedef struct send_data
 {
@@ -17,8 +23,11 @@ typedef struct send_data
 
 Udp_Net *gudpnet;
 FilesOperation *fo;
+FILES files;
+FILES *filesl;
 int main(int argc, char *argv[])
 {
+	//FILES *filesl = (FILES*)malloc(sizeof(FILES*));
 	printf("argc = %d, *argv[] = %s\n",argc ,argv[argc-1]);
 	if(argv[argc-1] == NULL)
 	{
@@ -26,8 +35,13 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 	fo->get_file_op(argv[argc-1]);
+	//memcpy(filesl,fo->get_file_op(argv[argc-1]),sizeof(FILES));
+	//printf("fo->get_file_op(argv[argc-1])->name = %s\n",fo->get_file_op(argv[argc-1])->name);
+	FILE *fp = fopen(argv[argc-1], "rb");
+	fread(files.data,sizeof(char),MAX_DATA_SIZE,fp);
+	//printf("files.data = %ld\n",filesl->data);	
 	Send *sed = new Send();
-	sed->start();
+	//sed->start();
 
 }
 
