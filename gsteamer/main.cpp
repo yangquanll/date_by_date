d299fcaf538bcd5593247dd03b311dd5a8908223
yangquan <yangquan@trustkernel.com>
2020-03-30 09:41:11 +0800

    update

--- gsteamer/main.cpp
+++ gsteamer/main.cpp
@@ -37,7 +37,7 @@ typedef struct _CustomData {
     QSlider *volumeSlider;
     QLabel  *curTimeLabel;
     QLabel  *durTimeLabel;
-    
+
     GstState state;                 /* Current state of the pipeline */
     gint64 duration;              /* Duration of the clip, in nanoseconds */
 
@@ -60,6 +60,12 @@ int main(int argc, char *argv[]){
     gst_init (&argc, &argv);    /* Initialize GStreamer */
     QApplication app(argc, argv);
 
+
+    /* Create our own GLib Main Context and make it the default one */
+    GMainContext* context = g_main_context_new ();
+    g_main_context_push_thread_default(context);
+
+    GST_DEBUG ("Creating pipeline");
     /* Create the empty pipeline */
     data.pipeline= gst_pipeline_new ("my_pipeline");
 
@@ -112,7 +118,7 @@ int main(int argc, char *argv[]){
     window.show();
     WId xwinid = window.get_movieFrame_winId();
 
-    gst_video_overlay_set_window_handle (GST_VIDEO_OVERLAY (data.vsink), xwinid); //设置播放窗口与视频叠加
+    gst_video_overlay_set_window_handle (GST_VIDEO_OVERLAY (data.vsink), xwinid); //???????????
     
     /* set custom data */
     data.duration = GST_CLOCK_TIME_NONE;
@@ -122,24 +128,25 @@ int main(int argc, char *argv[]){
 
     bus = gst_element_get_bus (data.pipeline);
     gst_bus_add_signal_watch (bus);
-    
+
     g_signal_connect (G_OBJECT (bus), "message::error", (GCallback)error_cb, &data);
     g_signal_connect (G_OBJECT (bus), "message::eos", (GCallback)eos_cb, &data);
     g_signal_connect (G_OBJECT (bus), "message::state-changed", (GCallback)state_changed_cb, &data);
     g_signal_connect (data.demuxer, "pad-added", G_CALLBACK (on_pad_added),  &data);
     g_timeout_add_seconds (1, (GSourceFunc)refresh_position, &data);
 
-	/* Set the source to play */
+    /* Set the source to play */
     g_object_set (G_OBJECT (data.source), "location", "./yq.webm", NULL);
 
     printf(" yq	 --> set path \n");
     /* Start playing */
     ret = gst_element_set_state (data.pipeline, GST_STATE_PLAYING);
-    if (ret == GST_STATE_CHANGE_FAILURE) 
+    if (ret == GST_STATE_CHANGE_FAILURE)
     {
         gst_element_set_state (data.pipeline, GST_STATE_NULL);
         gst_object_unref (data.pipeline);
-        //return -1;
+         g_printerr ("ERROR information: gst_element_set_state %sd\n",ret);
+        return -1;
     }
     return app.exec();
 }
@@ -148,7 +155,7 @@ static void error_cb (GstBus *bus, GstMessage *msg, CustomData *data)
 {
     GError *err;
     gchar *debug_info;
-    
+
     /* Print error details on the screen */
     gst_message_parse_error (msg, &err, &debug_info);
     g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
@@ -180,7 +187,7 @@ static void state_changed_cb (GstBus *bus, GstMessage *msg, CustomData *data)
 
         /*Refresh the GUI as soon as we reach the PAUSED state */
         if (old_state == GST_STATE_READY && new_state == GST_STATE_PAUSED)
-        { 
+        {
             refresh_position (data);
         }
     }
@@ -198,7 +205,7 @@ static gboolean refresh_position (CustomData *data)
         return TRUE;
 
     /* If we didn't know it yet, query the stream duration */
- #if 0
+ #if 1
     if (!GST_CLOCK_TIME_IS_VALID (data->duration))
     {
         if (!gst_element_query_duration (data->pipeline, fmt, &data->duration))
@@ -225,21 +232,23 @@ static gboolean refresh_position (CustomData *data)
 }
 
 static void on_pad_added (GstElement *element, GstPad *pad, CustomData *data )
-{     
-    gchar *pad_name;
+{
+    gchar *pad_name = NULL;
 
     g_object_get(pad, "name", &pad_name, NULL);
 
     if(g_str_has_prefix(pad_name, "video"))
     {
-        GstPad *v_sinkpad;
+        GstPad *v_sinkpad = NULL;
 
         v_sinkpad = gst_element_get_static_pad (data->vqueue, "sink");
-        if(!gst_pad_link (pad, v_sinkpad))
+        gst_pad_link (pad, v_sinkpad);
+
+/*        if(!gst_pad_link (pad, v_sinkpad))
             g_print("%s linked to vdecoder.\n",pad_name);
         else
             g_print("ERROR: gst_pad_link (pad, v_sinkpad), pad_name = %s\n",pad_name);
-
+*/
         gst_object_unref (v_sinkpad);
     }
 
