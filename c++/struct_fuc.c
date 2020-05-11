82a71baf699da90a0ff9b6a7d4ed7964f1a07e75
Yang Quan <yangquan@trustkernel.com>
2020-05-11 11:01:45 +0800

    fix crash

--- c++/struct_fuc.c
+++ c++/struct_fuc.c
@@ -5,23 +5,23 @@
 
 typedef int STRU_FU;
 
-STRU_FU fun1(int a, const char *b){
+STRU_FU sfun1(int a, const char *b){
 
 	printf("fun1  a = %d , b = %s \n",a,b);
 
 	return a;
 }
 
-STRU_FU fun2(int a, void *b,float c){
+STRU_FU sfun2(int a, void *b,float c){
 
 	printf("fun2  a = %d , b = %f \n",a,c);
 
 	return a;
 }
 
-STRU_FU fun3(int a, char b,long c){
+STRU_FU sfun3(int a, char b,long c){
 
-	printf("fun3  a = %d , b = %ld \n",a,c);
+	printf("fun3  a = %d , *b = %ld \n",a,c);
 	return a;
 }
 
@@ -34,15 +34,23 @@ struct fun_ops{
 
 struct fun_ops fuops = {
 
-	.fun1 = fun1,
-	.fun2 = fun2,
-	.fun3 = fun3,
+	.fun1 = sfun1,
+	.fun2 = sfun2,
+	.fun3 = sfun3,
 };
 
 int main(int argc,  char* argv[])
 {
 	int g;
-	struct fun_ops *ops = NULL; 
+	struct fun_ops *ops = NULL;
+
+	if(argc < 2)
+
+	{
+		printf("please input 1 or 2 or 3 for test fun \n");
+		return -1;
+	}
+
 	if(strcmp(argv[1], "1") == 0)
 	{
 		ops = &fuops;//赋值结构体
